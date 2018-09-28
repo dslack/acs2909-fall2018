e5f68b5584434a1302efb21499a5e8f3a2ee03ce
Dan Slack <ctharsis999@gmail.com>
2018-09-27 21:45:14 -0500

    updated to use a class to create lectures with events

--- 5.0/constants.js
+++ 5.0/constants.js
@@ -15,6 +15,36 @@ const TABS = [
 
 const LINKS = [{"label":"Slack Invite Link","link":"https://join.slack.com/t/acs2909-fall2018/shared_invite/enQtNDI4NTAxMTAzOTczLTY3ZWU3N2RhNDg0YjY0MTkzN2E2MzA4NzU0OTgxZTEyZjVhYjJjNjhlNDcwMWI5M2ZiM2YzZGU0MWQ5OGFmYmY"},{"label":"Eloquent Javascript (Course Textbook)","link":"http://eloquentjavascript.net/"},{"label":"Eloquent Javascript Code Samples","link":"https://eloquentjavascript.net/code/"},{"label":"JavaScript Design Patterns","link":"https://addyosmani.com/resources/essentialjsdesignpatterns/book/"},{"label":"Mozilla Developer Network","link":"https://developer.mozilla.org/en-US/docs/Web/JavaScript)"},{"label":"Why Learn Vanilla JS","link":"https://snipcart.com/blog/learn-vanilla-javascript-before-using-js-frameworks"},{"label":"JSFiddle","link":"http://jsfiddle.net"},{"label":"We might not need jQuery","link":"https://css-tricks.com/now-ever-might-not-need-jquery/"},{"label":"Visual Studio Code","link":"https://code.visualstudio.com/"},{"label":"Stackblitz","link":"https://stackblitz.com"},{"label":"Website Git Link","link":"https://github.com/dslack/acs2909-fall2018"}];
 
+const lecturePath = path => `https://courses.acs.uwinnipeg.ca/2909-050/lectures/${path}`;
+const examplesPath = path => `https://courses.acs.uwinnipeg.ca/2909-050/lectures/${path}`;
+
+const LECTURES = [
+    {
+        lectureLink:lecturePath("week01/Week01.pdf"),
+        lectureLabel: "Lecture 1",
+        lectureExamplesLink: examplesPath("week01/Week01.zip"),
+        lectureDetails: "Course Outline discussion, HTML/CSS"
+    },
+    {
+        lectureLink:lecturePath('week02/Week02.pdf'),
+        lectureLabel:"Lecture 2",
+        lectureExamplesLink: examplesPath("week02/Week02.zip"),
+        lectureDetails: "Finished CSS, Javascript language syntax, values/types, Variables, functions"
+    },
+    {
+        lectureLink: lecturePath('week03/Week03.pdf'),
+        lectureLabel:"Lecture 3",
+        lectureExamplesLink: examplesPath('week03/Week03.zip'),
+        lectureDetails: "More on functions, Rest parameters, Objects, Classes, prototypes"
+    },
+    {
+        lectureLink: lecturePath('week04/Week04.pdf'),
+        lectureLabel: "Lecture 4",
+        lectureExamplesLink: examplesPath('week04/Week04.zip'),
+        lectureDetails: "Events and DOM"
+    }
+]
+
 
 const Utilities = {
     setActiveTab: function (tab, link) {
--- 5.0/index.js
+++ 5.0/index.js
@@ -12,4 +12,25 @@ window.onload = function(){
         const links = document.getElementById('links');
         LINKS.forEach((linkObj) => links.appendChild(Utilities.createListLink(linkObj, true)));
     }
+
+    if (typeof lectureModule !== 'undefined') {
+        lectureModule.render();
+    }
+}
+
+window.addEventListener('keypress', handleKeyboardShortcuts);
+
+
+function handleKeyboardShortcuts(e) {
+    if (e.shiftKey && e.keyCode === 33) {
+        changeLocation(TABS[0]);
+    } else if (e.shiftKey && e.keyCode === 64) {
+        changeLocation(TABS[1]);
+    } else if (e.shiftKey && e.keyCode === 35) {
+        changeLocation(TABS[2]);
+    }
+}
+
+function changeLocation(tab) {
+    window.location = tab.link;
 }
